89a23e25b9d92a76c715f9cf5403806ef6b01579
Andreé Robles <61211800+andree-ro@users.noreply.github.com>
2024-05-10 22:41:42 -0600

    Add files via upload

---
+++ NÚMEROS DE MATRÍCULAS/vision-matricula-tarea.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Se usaron  métodos de tratamiento de imagen y visión artificial
+
+# ## Devuelva la cadena correspondiente al número de placa
+
+# In[1]:
+
+
+import cv2
+import pytesseract
+import matplotlib.pyplot as plt
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
+placa = []
+# Imagen de el carro con placa
+image = cv2.imread('auto.jpg')
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+gray = cv2.blur(gray,(3,3))
+canny = cv2.Canny(gray,150,200)
+canny = cv2.dilate(canny,None,iterations=1)
+cnts,_ = cv2.findContours(canny,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+for c in cnts:
+  area = cv2.contourArea(c)
+  x,y,w,h = cv2.boundingRect(c)
+  epsilon = 0.09*cv2.arcLength(c,True)
+  approx = cv2.approxPolyDP(c,epsilon,True)
+  
+  if len(approx)==4 and area>9000:
+    print('area=',area)
+    aspect_ratio = float(w)/h
+    if aspect_ratio>2.4:
+      placa = gray[y:y+h,x:x+w]
+      text = pytesseract.image_to_string(placa,config='--psm 11')
+      print('PLACA: ',text)
+      cv2.imshow('PLACA',placa)
+      cv2.moveWindow('PLACA',780,10)
+      cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0),3)
+      cv2.putText(image,text,(x-20,y-10),1,2.2,(0,255,0),3)
+      
+cv2.imshow('Image',image)
+cv2.moveWindow('Image',45,10)
+cv2.waitKey(0)
+
+
+# In[2]:
+
+
+plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB));
+
+
+# In[ ]:
+
+
+
+
